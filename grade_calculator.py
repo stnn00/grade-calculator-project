86ab1acc5a55e63dba076c0d66fef31709018c7b
stnn00 <nguyens6@csp.edu>
2025-09-06 18:41:34 -0500

    Initial commit: Set up project before adding additional features

---
+++ grade_calculator.py
@@ -0,0 +1,54 @@
+# Python dictionary to store student name and grade data
+student_grades = {
+    "Anna Nguyen": [80, 83, 87],
+    "Kaitlyn Nguyen": [89, 91, 98],
+    "Jason Nguyen": [90, 80, 84],
+    "Erick Santos": [68, 71, 70],
+    "Zheng Yongkang": [90, 91, 89],
+    "Benjy Fish": [71, 70, 70],
+    "Lebron James": [85, 81, 88],
+    "Jenna Ortega": [81, 74, 70],
+    "Emma Myers": [80, 82, 74],
+    "Zachary Patrone": [12, 10, 22],
+    "Mia Crossing": [30, 10, 44],
+    "KJ Apa": [81, 74, 89]
+}
+
+student_averages = {} # Creating empty dictionary to fill with new calculated data
+class_count = len(student_grades) # Using len() to calculate how many key-value pairs (students) are in student_grades
+student_letter_grades = {} # Creating empty dictionary to fill with new calculated data
+highest_average = 0
+top_student = ""
+passing_students = 0
+
+for student, grade in student_grades.items(): # Iterating student_grades dictionary so I can work with each student data individually
+    average_score = int(sum(grade) / len(grade)) # Using sum() to find the sum of each student's grades, then using / len() to divide by how many grades they have (3), int() turns the float (number with a decimal) into an integer (whole number)
+    student_averages[student] = average_score # Adding individual average scores into student_averages dictionary
+
+for student, average in student_averages.items():
+    if 90 <= average <= 100:
+        letter_grade = "A"
+    elif 80 <= average <= 89:
+        letter_grade = "B"
+    elif 70 <= average <= 79:
+        letter_grade = "C"
+    elif 60 <= average <= 69:
+        letter_grade = "D"
+    else:
+        letter_grade = "F"
+
+    student_letter_grades[student] = letter_grade
+
+    if average > highest_average:
+        highest_average = average
+        top_student = student
+    if average >= 70:
+        passing_students += 1
+
+list_averages = list(student_averages.values()) # Using list() to add all dictionary values into a list
+total_sum = (sum(list_averages)) # Using sum() to total all the values (grade averages)
+overall_average = int(total_sum / class_count) # Using int() to turn the float into an integer (whole number), can also use round() depending on grading system
+
+print(f"The top performing student is {top_student} with the highest class average of {highest_average}!")
+print(f"The overall class average is {overall_average}.")
+print(f"{passing_students} students successfully passed the course!")
